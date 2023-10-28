40ee46583a1fbbe29e5d864a445336f743433943
kadoOo <hkadir427@gmail.com>
2023-10-28 10:58:15 +0300

    Parola Olusturucu
    Kullanıcıdan uzunluk ve seviye değerleri alınıyor. Seçilebilecek karakterlere seviye birde harfler, ikide sayılar ve üçte özel karakterler ekleniyor. Sonra bu karakterler içinden belirlenen uzunluk kadarı rastgele alınıp döndürülüyor.

---
+++ parola-olusturucu.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+def generate_password(length, level, output=[]):
+    chars = string.ascii_letters
+    if level > 1:
+        chars = "{}{}".format(chars, string.digits)
+    if level > 2:
+        chars = "{}{}".format(chars, string.punctuation)
+    
+    for i in range(length):
+        output.append(random.choice(chars))
+    
+    return "".join(output)
+
+print(("-" * 30) + "\n Parola Oluşturucu\n" + ("-" * 30))
+
+password_length = int(input("Uzunluk: "))
+password_level = int(input("Seviye: "))
+
+password = generate_password(password_length, password_level)
+print("\nSenin Parolan: {}".format(password))
